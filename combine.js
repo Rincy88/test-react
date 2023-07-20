8844b71a1ec3d53cef902ffdc1bdfa9a38e66a8c
Rincy@d3v <98811049+Rincy88@users.noreply.github.com>
2023-07-20 12:40:42 +0530

    Update combine.js

--- combine.js
+++ combine.js
@@ -3,18 +3,19 @@ const fs = require('fs');
 // Read the first JSON file passed as the first argument
 const filePath1 = process.argv[2];
 const fileContents1 = fs.readFileSync(filePath1, 'utf8');
-const jsonObject1 = JSON.parse(fileContents1);
+//const jsonObject1 = JSON.parse(fileContents1);
 
 // Read the second JSON file passed as the second argument
-const filePath2 = process.argv[3];
-const fileContents2 = fs.readFileSync(filePath2, 'utf8');
-const jsonObject2 = JSON.parse(fileContents2);
+//const filePath2 = process.argv[3];
+//const fileContents2 = fs.readFileSync(filePath2, 'utf8');
+//const jsonObject2 = JSON.parse(fileContents2);
 
 // Combine the JSON objects
-const combinedObject = { ...jsonObject1, ...jsonObject2 };
+//const combinedObject = { ...jsonObject1, ...jsonObject2 };
 
+const encodedSbom=btoa(unescape(encodeURIComponent(JSON.stringify(fileContents1))))
 // Output the combined JSON object
-console.log(JSON.stringify(combinedObject, null, 2));
+//console.log(JSON.stringify(combinedObject, null, 2));
 
 // Write the combined JSON object to a new file
-fs.writeFileSync('combined.json', JSON.stringify(combinedObject, null, 2));
+fs.writeFileSync('converted_file.json', JSON.stringify(encodedSbom, null, 2));
