6b68aefda0f92a0f5ccbeb1e85aed0526c35b77a
Rincy@d3v <98811049+Rincy88@users.noreply.github.com>
2023-07-20 15:03:52 +0530

    Update combine.js

--- combine.js
+++ combine.js
@@ -4,7 +4,8 @@ const fs = require('fs');
 const filePath1 = process.argv[2];
 const fileContents1 = fs.readFileSync(filePath1, 'utf8'); 
 
-const encodedSbom=btoa(unescape(encodeURIComponent(JSON.stringify(fileContents1)))) 
+#const encodedSbom=btoa(unescape(encodeURIComponent(JSON.stringify(fileContents1)))) 
+const encodedSbom=Buffer.from(fileContents1).toString('base64')
 
 // Write the combined JSON object to a new file
 fs.writeFileSync('converted_file.json', JSON.stringify(encodedSbom, null, 2));
